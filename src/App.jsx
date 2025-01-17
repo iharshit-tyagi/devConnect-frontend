07944041b7cccf0cf07759734e7161251d6b2a5c
Harshit <harshitt52@gmail.com>
2025-01-17 16:21:28 +0530

    Added Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import NavBar from "./components/NavBar";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./components/Login";
+import Body from "./components/Body";
+import Signup from "./components/Signup";
+import Feed from "./components/Feed";
 function App() {
   return (
     <div>
-      <NavBar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/" element={<Login />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/feed" element={<Feed />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
