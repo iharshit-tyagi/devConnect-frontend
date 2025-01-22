1ce9775a547cb488ba140a0eb595a487a897d62d
Harshit <harshitt52@gmail.com>
2025-01-22 21:04:07 +0530

    fix accesstoken cookie

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
-
+import axios from 'axios';
+import { useNavigate } from "react-router-dom";
 const Login = () => {
 
 const[password,setPassword]=useState('');
 const[username,setUsername]=useState('');
-
+const [errMessage,setErrorMessage]=useState('');
+const navigate=useNavigate();
 const handleUsernameChange=(e)=>{  
   setUsername(e?.target?.value);
 }
@@ -12,14 +14,28 @@ const hanldePasswordChange=(e)=>{
 setPassword(e?.target?.value);
 }
 
-const hanldeLoginClick=()=>[
+const hanldeLoginClick=async ()=>{
+  try{
+    const res= await axios.post('http://localhost:3000/api/v1/user/signin',{
+   username,password
+    },  { headers: { 'Content-Type': 'application/json' },withCredentials:true });
+
+    navigate('/feed')
+    // setErrorMessage('');
+  }catch(err){
+    console.log(err);
+    setErrorMessage(err?.response?.data?.message)
+  }
+}
+
+
 //make api call here
-]
+
   return (
     <div className=" flex justify-center my-4">
 
     <div className="card bg-base-300 w-96 shadow-xl">
-  <div className="card-body">
+      <div className="card-body">
     <h2 className="card-title">Login</h2>
     <div>
    
@@ -33,7 +49,7 @@ const hanldeLoginClick=()=>[
     <path
       d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
   </svg>
-  <input type="text" className="grow" placeholder="Username or Email"  onChange={handleUsernameChange}/>
+  <input type="text" className="grow" placeholder="Username or Email" value={username}  onChange={handleUsernameChange}/>
 </label>
 <label className="  mt-2 input input-bordered flex items-center gap-2">
   <svg
@@ -46,9 +62,9 @@ const hanldeLoginClick=()=>[
       d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
       clipRule="evenodd" />
   </svg>
-  <input type="password" className="grow"  onChange={hanldePasswordChange} />
+  <input type="password" className="grow" value={password} placeholder="Password"  onChange={hanldePasswordChange} />
 </label>
-
+{errMessage && <p className="text-red-400 mt-3">{errMessage}</p>}
   </div>
     <div className="card-actions justify-center mt-3">
       <button className="btn btn-primary" onClick={hanldeLoginClick}>Login</button>
