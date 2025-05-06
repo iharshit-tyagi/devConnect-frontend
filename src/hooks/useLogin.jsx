cd7553bf295ed48b8ecd992886d33248d87091f5
Harshit <harshitt52@gmail.com>
2025-05-06 21:56:31 +0530

    Added UseLogin Hook

---
+++ src/hooks/useLogin.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom";
+import axios from 'axios';
+export const UseLogin = () => {
+const [errMessage,setErrorMessage]=useState('');
+const navigate=useNavigate();
+
+
+ const handleLogin=async (username,password)=>{
+    try{
+      const res= await axios.post('http://localhost:3000/api/v1/user/signin/',{
+     username,password
+      },  { headers: { 'Content-Type': 'application/json' },withCredentials:true });
+  console.log(res);
+  
+      navigate('/feed')
+      // setErrorMessage('');
+    }catch(err){
+      console.log(err);
+      setErrorMessage(err?.response?.data?.message)
+    }
+  }
+  return { handleLogin, errMessage };
+}
