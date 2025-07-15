106341cb1e9a6cffe5de5d452de0465ae062478b
Harshit <harshitt152@gmail.com>
2025-07-15 11:41:09 +0530

    Get User if signed in

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -2,12 +2,25 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useDispatch } from "react-redux";
 import { addUsers } from "../utils/userListSlice";
+import { addUser } from "../utils/userSlice";
 const Feed = () => {
   const [userList, setUserList] = useState([]);
   const dispatch = useDispatch();
   useEffect(() => {
     getUserList();
+    getSignedInUser();
   }, []);
+  const getSignedInUser = async () => {
+    try {
+      const res = await axios.get("http://localhost:3000/api/v1/user/view", {
+        withCredentials: true,
+      });
+      dispatch(addUser(res?.data?.response));
+      // setUserList(res?.data?.response);
+    } catch (err) {
+      console.log(err);
+    }
+  };
   const getUserList = async () => {
     try {
       const res = await axios.get(
